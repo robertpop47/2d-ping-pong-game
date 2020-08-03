e60feebf64a4f550c50840da8510df9673ebb64f
Robert Pop <63066180+robertpop47@users.noreply.github.com>
2020-08-03 18:05:47 +0300

    Delete canvas.js

--- canvas.js
+++
@@ -1,4 +0,0 @@
-export const canvas = document.getElementById("myCanvas");
-export const ctx = canvas.getContext("2d");
-
-export const clearCanvas = () => { ctx.clearRect(0, 0, canvas.width, canvas.height); }
